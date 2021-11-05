497407c45095a9f9bf8dc5ffffbf1bb14bc4c9e2
narvikd <84069271+narvikd@users.noreply.github.com>
2021-11-06 00:53:58 +0100

    Added handler for msg endpoint, added "/ban" endpoint

--- telegrambot/endpoint/endpoints.go
+++ telegrambot/endpoint/endpoints.go
@@ -3,6 +3,7 @@ package endpoint
 import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"paraguero_reloaded/telegrambot/endpoint/welcome"
+	"paraguero_reloaded/telegrambot/handler"
 	"paraguero_reloaded/telegrambot/handler/onsticker"
 	"paraguero_reloaded/telegrambot/handler/ontext"
 	"paraguero_reloaded/telegrambot/handler/onuserjoin"
@@ -11,6 +12,7 @@ import (
 // LoadEndpoints loads all the endpoints, and it's where they're declared
 func LoadEndpoints(bot *tb.Bot) {
 	links(bot, "/links")
+	handler.MsgEndpoint(bot, "/ban", "Venga tonto, pa tu casa")
 	onuserjoin.AddEndpoint(bot, welcomeEndpoint.Welcome)
 	ontext.AddEndpoint(bot, paraguas)
 	onsticker.AddEndpoint(bot, paraguasSticker)
