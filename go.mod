cf09e042ccae0b417ebe79e1d982d5b084f6bc94
narvikd <84069271+narvikd@users.noreply.github.com>
2022-03-21 21:28:27 +0100

    Updated dependencies, simplified logger
    Signed-off-by: narvikd <84069271+narvikd@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,20 +1,19 @@
 module paraguero_reloaded
 
-go 1.17
+go 1.18
 
 require (
-	github.com/TwiN/go-color v1.0.1
+	github.com/TwiN/go-color v1.1.0
 	github.com/dustin/go-humanize v1.0.0
 	github.com/joho/godotenv v1.4.0
 	github.com/narvikd/timekit v1.2.1
 	github.com/pkg/errors v0.9.1
-	github.com/valyala/fasthttp v1.31.0
+	github.com/valyala/fasthttp v1.34.0
 	gopkg.in/tucnak/telebot.v2 v2.5.0
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.2 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
--- telegrambot/handler/logger/logger.go
+++ telegrambot/handler/logger/logger.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"paraguero_reloaded/telegrambot/handler"
 	"strconv"
-	"strings"
 )
 
 func Register(bot *tb.Bot, src *tb.Message) {
@@ -14,15 +13,11 @@ func Register(bot *tb.Bot, src *tb.Message) {
 }
 
 func Logger(src *tb.Message) {
-	if strings.HasPrefix(src.Text, "/") {
-		if src.Text != "/" {
-			chat := "\"" + color.Ize(color.Red, src.Chat.Title) + "\": "
-			user := color.Ize(color.Green, handler.MakeNewMention(src)) + ". "
-			used := "used \"" + color.Ize(color.Bold+color.Red, src.Text) + "\". "
-			chatID := "(ChatID: \"" + color.Ize(color.Purple, strconv.FormatInt(src.Chat.ID, 10)) + "\". "
-			chatType := "Chat type: " + color.Ize(color.Purple, string(src.Chat.Type)) + ") "
-			str := chat + user + used + chatID + chatType
-			log.Println(str)
-		}
-	}
+	chat := "\"" + color.Ize(color.Red, src.Chat.Title) + "\": "
+	user := color.Ize(color.Green, handler.MakeNewMention(src)) + ". "
+	used := "said \"" + color.Ize(color.Bold+color.Red, src.Text) + "\". "
+	chatID := "(ChatID: \"" + color.Ize(color.Purple, strconv.FormatInt(src.Chat.ID, 10)) + "\". "
+	chatType := "Chat type: " + color.Ize(color.Purple, string(src.Chat.Type)) + ") "
+	str := chat + user + used + chatID + chatType
+	log.Println(str)
 }
